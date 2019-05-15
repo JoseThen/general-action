26d3963ae459fb67b75150cfdbc1a813f8fe4b64
Jose Then <jthen@groupninemedia.com>
2019-05-15 10:50:23 -0400

    Remove SSH, add more utility

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,4 @@
 
 set -e
 
-SSH_PATH="$HOME/.ssh"
-
-mkdir "$SSH_PATH"
-touch "$SSH_PATH/known_hosts"
-
-echo "$PRIVATE_KEY" > "$SSH_PATH/github_key"
-echo "$PUBLIC_KEY" > "$SSH_PATH/github_key.pub"
-
-chmod 700 "$SSH_PATH"
-chmod 600 "$SSH_PATH/known_hosts"
-chmod 600 "$SSH_PATH/github_key"
-chmod 600 "$SSH_PATH/github_key.pub"
-
-eval $(ssh-agent)
-ssh-add "$SSH_PATH/github_key"
-
-ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
-
 sh -c "$*"
